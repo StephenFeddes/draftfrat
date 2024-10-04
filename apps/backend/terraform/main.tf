de4b4e99c911b237da700b0a29e733f94c298292
StephenFeddes <feddessteve@gmail.com>
2024-10-03 19:10:52 -0500

    Testing CICD.

--- apps/backend/terraform/main.tf
+++ apps/backend/terraform/main.tf
@@ -1,32 +1,32 @@
-
+# Cloudflare Pages Project
 resource "cloudflare_pages_project" "frontend_project" {
-  account_id = var.cloudflare_account_id
-  name       = "rosterroyale-pages"
+  account_id       = var.cloudflare_account_id
+  name              = "rosterroyale-pages"
   production_branch = "main"
 
   build_config {
     build_command   = "npm install && npm run build"
     destination_dir = "dist"
-    root_dir       = "apps/frontend/web"
+    root_dir        = "apps/frontend/web"
   }
 }
 
 # Cloudflare DNS Record for Root Domain (rosterroyale.com)
-resource "cloudflare_dns_record" "default_domain" {
+resource "cloudflare_record" "default_domain" {
   zone_id = data.cloudflare_zones.default.zones[0].id
-  name    = "" # This represents the root domain (rosterroyale.com)
-  value   = "${cloudflare_pages_project.frontend_project.name}.pages.dev" # Use the project's name to construct the URL
-  type    = "CNAME"
-  ttl     = 3600
-  proxied = false # You can set this to true if you want Cloudflare to manage caching and performance
+  name     = "" # This represents the root domain (rosterroyale.com)
+  value    = "${cloudflare_pages_project.frontend_project.name}.pages.dev" # Use the project's name to construct the URL
+  type     = "CNAME"
+  ttl      = 3600
+  proxied  = false # You can set this to true if you want Cloudflare to manage caching and performance
 }
 
 # Cloudflare DNS Record for www Subdomain (www.rosterroyale.com)
-resource "cloudflare_dns_record" "www_domain" {
+resource "cloudflare_record" "www_domain" {
   zone_id = data.cloudflare_zones.default.zones[0].id
-  name    = "www" # This represents the www subdomain (www.rosterroyale.com)
-  value   = "${cloudflare_pages_project.frontend_project.name}.pages.dev" # Use the project's name to construct the URL
-  type    = "CNAME"
-  ttl     = 3600
-  proxied = false # Same as above, set to true if desired
+  name     = "www" # This represents the www subdomain (www.rosterroyale.com)
+  value    = "${cloudflare_pages_project.frontend_project.name}.pages.dev" # Use the project's name to construct the URL
+  type     = "CNAME"
+  ttl      = 3600
+  proxied  = false # Same as above, set to true if desired
 }
