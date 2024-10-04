369304015aff1f178b8820e96f1da61bb9dbe4f6
StephenFeddes <feddessteve@gmail.com>
2024-10-04 13:36:52 -0500

    Testing CICD.

--- apps/backend/terraform/main.tf
+++ apps/backend/terraform/main.tf
@@ -1,32 +1,36 @@
-# Cloudflare Pages Project
-resource "cloudflare_pages_project" "frontend_project" {
-  account_id       = var.cloudflare_account_id
-  name              = "rosterroyale-pages"
-  production_branch = "main"
-
-  build_config {
-    build_command   = "npm install && npm run build"
-    destination_dir = "dist"
-    root_dir        = "apps/frontend/web"
+# Create Google Cloud Storage Bucket
+resource "google_storage_bucket" "frontend_bucket" {
+  name     = "frontend_bucket"
+  location = var.region
+  website {
+    main_page_suffix = "apps/frontend/web/dist/index.html"
   }
+
+  # Make the bucket public
+  uniform_bucket_level_access = true
 }
 
-# Cloudflare DNS Record for Root Domain (rosterroyale.com)
-resource "cloudflare_record" "default_domain" {
+# Configure bucket access to be public
+resource "google_storage_bucket_iam_member" "allUsers" {
+  bucket = google_storage_bucket.frontend_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+# Cloudflare DNS record for your domain pointing to the Google Storage bucket via Cloudflare
+resource "cloudflare_record" "root_domain" {
   zone_id = data.cloudflare_zones.default.zones[0].id
-  name     = "@" # Use "@" to represent the root domain (rosterroyale.com)
-  value    = "${cloudflare_pages_project.frontend_project.name}.pages.dev" # Use the project's name to construct the URL
-  type     = "CNAME"
-  ttl      = 3600
-  proxied  = false # You can set this to true if you want Cloudflare to manage caching and performance
+  name    = var.domain_name
+  value   = google_storage_bucket.frontend_bucket.self_link
+  type    = "CNAME"
+  proxied = false
 }
 
-# Cloudflare DNS Record for www Subdomain (www.rosterroyale.com)
+# Optional: Cloudflare DNS record for www subdomain (e.g., www.rosterroyale.com)
 resource "cloudflare_record" "www_domain" {
   zone_id = data.cloudflare_zones.default.zones[0].id
-  name     = "www" # This represents the www subdomain (www.rosterroyale.com)
-  value    = "${cloudflare_pages_project.frontend_project.name}.pages.dev" # Use the project's name to construct the URL
-  type     = "CNAME"
-  ttl      = 3600
-  proxied  = false # Same as above, set to true if desired
+  name    = "www"
+  value   = google_storage_bucket.frontend_bucket.self_link
+  type    = "CNAME"
+  proxied = false
 }
