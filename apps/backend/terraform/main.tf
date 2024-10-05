cfcb1068180a17c203b9d2aa26a2233f20ee25c7
StephenFeddes <feddessteve@gmail.com>
2024-10-05 16:01:42 -0500

    Testing CICD.

--- apps/backend/terraform/main.tf
+++ apps/backend/terraform/main.tf
@@ -1,4 +1,3 @@
-
 # Create Google Cloud Storage Bucket
 resource "google_storage_bucket" "rosterroyale_frontend_bucket" {
   name     = "rosterroyale.com" # Make sure to verify your domain with Google Search Console
