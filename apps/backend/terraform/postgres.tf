7e0db58b96c1b682f4dd121aee93fa223bcfe089
StephenFeddes <feddessteve@gmail.com>
2024-10-16 15:04:44 -0500

    Testing CICD.

--- apps/backend/terraform/postgres.tf
+++ apps/backend/terraform/postgres.tf
@@ -1,13 +1,7 @@
-# Random password generator for the database user
-resource "random_password" "pg_password" {
-  length  = 16
-  special = true
-  override_special = "_%@"
-}
-
-# Create a Neon Project
+# Create a Neon Project with valid history retention seconds
 resource "neon_project" "default" {
-  name = "DraftFratProject"
+  name                       = "DraftFratProject"
+  history_retention_seconds  = 86400  # Maximum allowed value
 }
 
 # Create a branch in the Neon project
@@ -33,6 +27,6 @@ resource "neon_database" "users_db" {
 
 # Output the connection string
 output "postgres_connection_string" {
-    value = neon_project.default.connection_uri
-    sensitive = true
+  value     = neon_project.default.connection_uri
+  sensitive = true
 }
