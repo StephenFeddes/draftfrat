b228d0a6c1a32884fe8093ac72d45f1ef297fc2e
StephenFeddes <feddessteve@gmail.com>
2024-10-16 15:14:46 -0500

    Testing CICD.

--- apps/backend/terraform/postgres.tf
+++ apps/backend/terraform/postgres.tf
@@ -4,25 +4,10 @@ resource "neon_project" "default" {
   history_retention_seconds  = 86400  # Maximum allowed value
 }
 
-# Create a branch in the Neon project
-resource "neon_branch" "production" {
-  project_id = neon_project.default.id
-  name       = "production"
-}
-
-# Create a Neon Role (database user) and assign the generated password
-resource "neon_role" "db_user" {
-  project_id = neon_project.default.id
-  branch_id  = neon_branch.production.id
-  name       = "draftfrat_user"
-}
-
 # Create a Neon Database in the project
 resource "neon_database" "users_db" {
   project_id = neon_project.default.id
-  branch_id  = neon_branch.production.id
   name       = "users"
-  owner_name = neon_role.db_user.name
 }
 
 # Output the connection string
