636519dc321675bd6f1c35965e92d737b7eed4a3
StephenFeddes <feddessteve@gmail.com>
2024-10-16 14:01:39 -0500

    Testing CICD.

--- apps/backend/terraform/postgres.tf
+++ apps/backend/terraform/postgres.tf
@@ -8,17 +8,16 @@ resource "random_password" "pg_password" {
 # Create a Neon Project
 resource "neon_project" "default" {
   name = "DraftFratProject"
-  region = "us-east-1"
 }
 
 # Create a Neon Database in the project
-resource "neon_project_database" "users_db" {
+resource "neon_database" "users_db" {
   project_id = neon_project.default.id
   name       = "users"
 }
 
 # Create a Neon Role (database user) and assign the generated password
-resource "neon_project_role" "db_user" {
+resource "neon_role" "db_user" {
   project_id = neon_project.default.id
   name       = "draftfrat_user"
   password   = random_password.pg_password.result
