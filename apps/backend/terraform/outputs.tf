e2bb73d5b63e5605b4a4d1ad0aa7445bef81db2f
StephenFeddes <feddessteve@gmail.com>
2024-10-16 14:08:22 -0500

    Testing CICD.

--- apps/backend/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "postgres_connection_string" {
-  value = "postgresql://${neon_project_role.db_user.name}:${random_password.pg_password.result}@${neon_project.default.host}/${neon_project_database.default_db.name}?sslmode=require"
-}
--- apps/backend/terraform/postgres.tf
+++ apps/backend/terraform/postgres.tf
@@ -10,15 +10,34 @@ resource "neon_project" "default" {
   name = "DraftFratProject"
 }
 
-# Create a Neon Database in the project
-resource "neon_database" "users_db" {
+# Create a branch in the Neon project
+resource "neon_branch" "main" {
   project_id = neon_project.default.id
-  name       = "users"
+  name       = "main"
 }
 
 # Create a Neon Role (database user) and assign the generated password
 resource "neon_role" "db_user" {
   project_id = neon_project.default.id
+  branch_id  = neon_branch.main.id
   name       = "draftfrat_user"
   password   = random_password.pg_password.result
+}
+
+# Create a Neon Database in the project
+resource "neon_database" "users_db" {
+  project_id = neon_project.default.id
+  branch_id  = neon_branch.main.id
+  name       = "users"
+  owner_name = neon_role.db_user.name
+}
+
+# Output the connection string
+output "postgres_connection_string" {
+  value = format("postgresql://%s:%s@%s/%s?sslmode=require", 
+    neon_role.db_user.name, 
+    random_password.pg_password.result,
+    neon_project.default.connection_uri,
+    neon_database.users_db.name)
+  sensitive = true
 }
