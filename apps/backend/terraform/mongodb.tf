87c73ba3644527ba0d78150e942653fa4fff0e28
StephenFeddes <feddessteve@gmail.com>
2024-10-17 20:36:12 -0500

    Testing CICD

--- apps/backend/terraform/mongodb.tf
+++ apps/backend/terraform/mongodb.tf
@@ -3,12 +3,21 @@ resource "mongodbatlas_project" "default" {
   org_id = "65e4c9963d64375d8e02a9d7"
 }
 
-resource "mongodbatlas_cluster" "default" {
+resource "mongodbatlas_advanced_cluster" "default" {
   project_id                 = mongodbatlas_project.default.id
   name                       = "draftfrat-cluster"
-  provider_name              = "AWS"
-  provider_region_name       = "US_EAST_1"
-  provider_instance_size_name = "M0"
+  cluster_type               = "REPLICASET"
+  replication_specs {
+    region_configs {
+        electable_specs {
+            instance_size = "M0"
+        }
+        provider_name = "TENANT"
+        backing_provider_name = "AWS"
+        region_name = "US_EAST_1"
+        priority = 1
+    }
+  }
 }
 
 resource "mongodbatlas_project_ip_access_list" "ip" {
