a5faa7e52c0dd5f70f5ad5ae34022c5909d71814
StephenFeddes <feddessteve@gmail.com>
2024-10-18 00:44:13 -0500

    Testing CICD.

--- apps/backend/terraform/mongodb.tf
+++ apps/backend/terraform/mongodb.tf
@@ -49,6 +49,6 @@ resource "mongodbatlas_database_user" "default" {
 }
 
 output "connection_string" {
-  value = "mongodb+srv://${mongodbatlas_database_user.default.username}:test@${mongodbatlas_advanced_cluster.default.name}.${mongodbatlas_advanced_cluster.default.id}.mongodb.net/?retryWrites=true&w=majority"
-  sensitive = false
+  value = "mongodb+srv://${mongodbatlas_database_user.default.username}:test@${replace(mongodbatlas_advanced_cluster.default.connection_strings[0].standard_srv, "mongodb+srv://", "")}/?retryWrites=true&w=majority"
+  sensitive = true
 }
